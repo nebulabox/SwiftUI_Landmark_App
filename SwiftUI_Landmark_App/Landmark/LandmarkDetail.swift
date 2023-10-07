daab8b6862bc4da3d1ab4d7e42b6031f924a401c
nebulabox <nebulabox@gmail.com>
2023-10-07 15:18:30 +0800

    update

--- SwiftUI_Landmark_App/Landmark/LandmarkDetail.swift
+++ SwiftUI_Landmark_App/Landmark/LandmarkDetail.swift
@@ -52,5 +52,6 @@ struct LandmarkDetail: View {
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+            .environmentObject(UserData())
     }
 }
